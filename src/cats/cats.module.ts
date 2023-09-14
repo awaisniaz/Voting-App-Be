a57d895c9399a3b3ecd53880f48795ed010bcd41
awais.niaz@mwanmobile.org <awais.niaz@mwanmobile.org>
2023-09-14 19:37:32 +0500

    Remove vote and user  === Modules

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,7 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
-import { LoggerMiddleware } from 'src/middleware/logger.middleware';
+import { LoggerMiddleware } from 'src/Middleware/logger.middleware';
 
 @Module({
   controllers: [CatsController],

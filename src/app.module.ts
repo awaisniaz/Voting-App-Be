696cc0937fbee37866fd786476c782a1f5a8fe05
awais.niaz@mwanmobile.org <awais.niaz@mwanmobile.org>
2023-09-18 16:04:29 +0500

    --interceptor Implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptable } from './Interceptor/logging.interceptor';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService,],
+  providers: [AppService, {
+    provide: APP_INTERCEPTOR, useClass: LoggingInterceptable
+  }],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-
+import { LoggingInterceptable } from './Interceptor/logging.interceptor';
 async function VotingApp() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     abortOnError: false,
